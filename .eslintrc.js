67b54608d3964e50aac6082f375bca602ae23db3
sapics <gv.nishino@gmail.com>
2018-11-13 20:22:29 +0900

    Fix

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "parserOptions": {
+        "parser": "typescript-eslint-parser"
+    },
+    "extends": [
+        "eslint:recommended",
+        "typescript"
+    ],
+    "rules": {
+        "indent": ["error", 2],
+        "no-undef": "error",
+    }
+};
---
+++ __commit_push.bat
@@ -0,0 +1,4 @@
+git add *.js
+git add *.ts
+git commit -m "Fix"
+git push origin master
